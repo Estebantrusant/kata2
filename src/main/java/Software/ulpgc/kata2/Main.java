ff492a6d4234fb201e4d9270bb8f251929047b59
Estebantrusant <estebita.trujillo@gmail.com>
2025-10-23 12:15:08 +0100

    Class Main created

---
+++ src/main/java/Software/ulpgc/kata2/Main.java
@@ -0,0 +1,13 @@
+package Software.ulpgc.kata2;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        RemoteMovieLoader remoteMovieLoader = new RemoteMovieLoader();
+        List<Movie> movies = remoteMovieLoader.loadAll();
+        for (Movie movie : movies) {
+            System.out.println(movie);
+        }
+    }
+}
